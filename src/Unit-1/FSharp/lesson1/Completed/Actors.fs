300dbe93708709dc2f64a87d697f98a995e8080c
Stephen Kennedy <stephen@devenable.com>
2015-03-22 12:16:55 +1100

    Lesson 1.2 completed

--- src/Unit-1/FSharp/lesson1/Completed/Actors.fs
+++ src/Unit-1/FSharp/lesson1/Completed/Actors.fs
@@ -8,15 +8,48 @@ open Akka.FSharp
 let ExitCommand = "exit"
 
 [<Literal>]
+let StartCommand = "start"
+
+[<Literal>]
 let ContinueCommand = "continue"
 
+[<Literal>]
+let EmptyCommand = ""
+
 let consoleReaderActor (consoleWriter: ActorRef) (mailbox: Actor<_>) message = 
-    let line = Console.ReadLine ()
-    match line.ToLower () with
-    | ExitCommand -> mailbox.Context.System.Shutdown ()
-    | _ -> 
-        consoleWriter <! line
-        mailbox.Self  <! ContinueCommand
+    
+    let (|ValidMessage|_|) (msg : string) = if msg.Length % 2 = 0 then Some msg else None
+        
+    let doPrintInstructions() =
+        Console.WriteLine "Write whatever you want into the console!"
+        Console.WriteLine "Some entries will pass validation, and some won't...\n\n"
+        Console.WriteLine "Type 'exit' to quit this application at any time.\n"
+
+    let getAndValidateInput() =
+        let message = Console.ReadLine()
+
+        match message with
+        | EmptyCommand -> consoleWriter <! InputError("No input received.", NullInput)
+        | ExitCommand -> mailbox.Context.System.Shutdown()
+        | ValidMessage _ -> 
+            consoleWriter <! InputSuccess "Thank you mesage was valid"
+            mailbox.Self <! ContinueProcessing
+        | _ -> 
+            consoleWriter <! InputError("Invalid: input had odd number of characters.", Validation)
+            mailbox.Self <! ContinueProcessing
+
+    match box message with
+    | :? string as command ->
+        match command with
+        | StartCommand -> doPrintInstructions()
+        | _ -> ()
+    | :? InputResult as result -> 
+        match result with
+        | InputError(_, _) as error -> consoleWriter <! error
+        | _ -> ()
+    | _  -> ()
+    
+    getAndValidateInput()
 
 let consoleWriterActor message = 
     let (|Even|Odd|) n = if n % 2 = 0 then Even else Odd
@@ -26,7 +59,13 @@ let consoleWriterActor message =
         Console.WriteLine (message.ToString ())
         Console.ResetColor ()
 
-    match message.ToString().Length with
-    | 0    -> printInColor ConsoleColor.DarkYellow "Please provide an input.\n"
-    | Even -> printInColor ConsoleColor.Red "Your string had an even # of characters.\n"
-    | Odd  -> printInColor ConsoleColor.Green "Your string had an odd # of characters.\n"
+    match box message with
+    | :? InputResult as inputResult ->
+        match inputResult with
+        | InputError(msg, _) ->
+            printInColor ConsoleColor.Red msg
+        | InputSuccess msg ->
+            printInColor ConsoleColor.Green msg
+    | _ -> 
+        message.ToString() 
+        |> printInColor ConsoleColor.DarkYellow 
---
+++ src/Unit-1/FSharp/lesson1/Completed/Messages.fs
@@ -0,0 +1,13 @@
+﻿[<AutoOpen>]
+module Messages
+
+type ProcessCommand =
+| ContinueProcessing
+
+type ErrorType =
+| NullInput
+| Validation
+
+type InputResult =
+| InputSuccess of string
+| InputError of reason: string * errorType: ErrorType

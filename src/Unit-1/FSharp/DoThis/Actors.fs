6434f3baeb632f4d0b290f628d903826c145a19d
Stephen Kennedy <Stephen@Stephens-MacBook-Pro.local>
2015-04-05 20:39:53 +1000

    Lesson 1.3 completed

--- src/Unit-1/FSharp/DoThis/Actors.fs
+++ src/Unit-1/FSharp/DoThis/Actors.fs
@@ -5,18 +5,43 @@ open Akka.Actor
 open Akka.FSharp
 
 [<Literal>]
+let StartCommand = "start"
+
+[<Literal>]
 let ExitCommand = "exit"
 
 [<Literal>]
 let ContinueCommand = "continue"
 
-let consoleReaderActor (consoleWriter: ActorRef) (mailbox: Actor<_>) message = 
-    let line = Console.ReadLine ()
-    match line.ToLower () with
-    | ExitCommand -> mailbox.Context.System.Shutdown ()
-    | _ -> 
-        consoleWriter <! line
-        mailbox.Self  <! ContinueCommand
+[<Literal>]
+let EmptyCommand = ""
+
+let consoleReaderActor (validationActor: ActorRef) (mailbox: Actor<_>) message = 
+    let printInstructions() =
+        Console.WriteLine "Write whatever you want into the console!"
+        Console.WriteLine "Some entries will pass validation, and some won't...\n\n"
+        Console.WriteLine "Type 'exit' to quit this application at any time.\n"
+
+    let getAndValidateInput() =
+        match Console.ReadLine() with
+        | ExitCommand -> mailbox.Context.System.Shutdown()
+        | msg -> validationActor <! msg
+
+    match message.ToString().ToLower() with
+    | StartCommand -> printInstructions()
+    | _ -> ()
+
+    getAndValidateInput()
+
+let validationActor (consoleWriter : ActorRef) (mailbox : Actor<_>) message =
+    let (|ValidMessage|_|) msg = if (msg.ToString ()).Length % 2 = 0 then Some msg else None
+    
+    match message with
+    | EmptyCommand -> consoleWriter <! InputError("No input received.", ErrorType.Null)
+    | ValidMessage _ -> consoleWriter <! InputSuccess("Thank you! Message was valid.")
+    | _ -> consoleWriter <! InputError("Invalid: input had an odd number of characters.", ErrorType.Validation)
+
+    mailbox.Sender () <! ContinueProcessing
 
 let consoleWriterActor message = 
     let (|Even|Odd|) n = if n % 2 = 0 then Even else Odd
@@ -26,7 +51,9 @@ let consoleWriterActor message =
         Console.WriteLine (message.ToString ())
         Console.ResetColor ()
 
-    match message.ToString().Length with
-    | 0    -> printInColor ConsoleColor.DarkYellow "Please provide an input.\n"
-    | Even -> printInColor ConsoleColor.Red "Your string had an even # of characters.\n"
-    | Odd  -> printInColor ConsoleColor.Green "Your string had an odd # of characters.\n"
+    match box message with
+    | :? InputResult as inputResult ->
+        match inputResult with 
+        | InputError(reason,_) -> printInColor ConsoleColor.Red reason
+        | InputSuccess(reason) -> printInColor ConsoleColor.Green reason
+    | _ -> printInColor ConsoleColor.Black (message.ToString ())
--- src/Unit-1/FSharp/DoThis/Program.fs
+++ src/Unit-1/FSharp/DoThis/Program.fs
@@ -3,37 +3,23 @@ open Akka.FSharp
 open Akka.FSharp.Spawn
 open Akka.Actor
 
-let printInstructions () =
-    Console.WriteLine "Write whatever you want into the console!"
-    Console.Write "Some lines will appear as"
-    Console.ForegroundColor <- ConsoleColor.DarkRed
-    Console.Write " red "
-    Console.ResetColor ()
-    Console.Write " and others will appear as"
-    Console.ForegroundColor <- ConsoleColor.Green
-    Console.Write " green! "
-    Console.ResetColor ()
-    Console.WriteLine ()
-    Console.WriteLine ()
-    Console.WriteLine "Type 'exit' to quit this application at any time.\n"
-
 [<EntryPoint>]
 let main argv = 
     // initialize an actor system
     let myActorSystem = System.create "MyActorSystem" (Configuration.load ())
-    printInstructions ()
-    
+
     // make your first actors using the 'spawn' function
 
     let consoleWriterActor = 
-        actorOf Actors.consoleWriterActor
-        |> spawn myActorSystem "consoleWriterActor"
+        spawnOpt myActorSystem "consoleWriterActor" (actorOf Actors.consoleWriterActor) [SpawnOption.SupervisorStrategy(SupervisorStrategy.DefaultStrategy)]
 
-    let consoleReaderActor = 
-        actorOf2 (Actors.consoleReaderActor consoleWriterActor)
-        |> spawn myActorSystem "consoleReaderActor"
+    let validationActor =
+        spawnOpt myActorSystem "validationActor" (actorOf2 (Actors.validationActor consoleWriterActor)) [SpawnOption.Deploy(Deploy.Local)]
     
-    consoleReaderActor <! "start"
+    let consoleReaderActor = 
+        spawnOpt myActorSystem "consoleReaderActor" (actorOf2 (Actors.consoleReaderActor validationActor)) [SpawnOption.Router(Akka.Routing.RouterConfig.NoRouter)]
+    
+    consoleReaderActor <! Actors.StartCommand
 
     myActorSystem.AwaitTermination ()
     0

05541f3d0937564259d94ea26dc01f3762339e54
Stephen Kennedy <stephen@devenable.com>
2015-04-07 20:02:47 +1000

    Lesson 2.2 completed

--- src/Unit-2/FSharp/Actors.fs
+++ src/Unit-2/FSharp/Actors.fs
@@ -1,13 +1,29 @@
 ﻿module Actors
 
+open System
 open System.Windows.Forms.DataVisualization.Charting
 open Akka.Actor
 open Akka.FSharp
 
-let chartingActor (chart: Chart) message =
-    match message with
-    | InitializeChart series -> 
-        chart.Series.Clear ()
-        series |> Map.iter (fun k v -> 
-            v.Name <- k
-            chart.Series.Add(v))
+let chartingActor (chart: Chart) =
+    let seriesIndex = ref Map.empty  
+
+    let addSeries (series : Series) =
+        seriesIndex := (!seriesIndex).Add (series.Name, series)
+        chart.Series.Add series
+
+    (fun message -> 
+        match message with
+        | InitializeChart series -> 
+            chart.Series.Clear ()
+            series |> Map.iter (fun k v -> 
+                v.Name <- k
+                chart.Series.Add(v))
+        | AddSeries series when
+            not(String.IsNullOrEmpty series.Name) &&
+            not(!seriesIndex |> Map.containsKey series.Name) ->
+                addSeries series
+        | _ -> ()
+    )
+
+    
--- src/Unit-2/FSharp/Messages.fs
+++ src/Unit-2/FSharp/Messages.fs
@@ -4,5 +4,6 @@ module Messages
 open System.Collections.Generic
 open System.Windows.Forms.DataVisualization.Charting
 
-type InitializeChart = 
+type ChartMessage = 
 | InitializeChart of initialSeries: Map<string, Series>
+| AddSeries of series: Series
